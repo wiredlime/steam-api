5fa2dcf5a2f5c5a225241f24102e7101ae682548
luke <lukehoang195@gmail.com>
2022-02-07 17:59:53 +0700

    extract tags

---
+++ server/extractTagGenre.js
@@ -0,0 +1,19 @@
+/**
+ * For making genres and tags
+ */
+// const genreSet = new Set();
+// const tagSet = new Set();
+// result.forEach(({ genres, steamspy_tags }) => {
+//   console.log(genres, steamspy_tags);
+//   genres.forEach((e) => {
+//     if (e.length > 1) genreSet.add(e);
+//   });
+//   [...steamspy_tags].forEach((e) => {
+//     if (e.length > 1) tagSet.add(e);
+//   });
+// });
+// const genres = Array.from(genreSet);
+// const tags = Array.from(tagSet);
+
+// await db.collection("genres").insert({ genres, count: genres.length });
+// await db.collection("steamspy_tags").insert({ tags, count: tags.length });
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,13 +1,62 @@
 const express = require("express");
+const { set } = require("mongoose");
 const router = express.Router();
 const mongodbutil = require("../mongo/mongoUtil");
 let db = mongodbutil.getDb();
 
 router.get("/", async (req, res, next) => {
-  const dba = await db.collection("games");
-  const result = await dba.find().toArray();
+  res.send("Welcome to CoderSchool's Steam api");
+});
+
+router.get("/games", async (req, res, next) => {
+  let { page, limit, sortBy, ...filter } = req.query;
+  // process pagination
+  page = parseInt(page) || 1;
+  limit = parseInt(limit) || 10;
+
+  // process filter and search
+  for (const [key, value] of Object.entries(filter)) {
+    if (value) {
+      switch (key) {
+        case "search":
+          filter.name = new RegExp(
+            value
+              .split(" ")
+              .map((e) => {
+                e[0].toUpperCase();
+                return e;
+              })
+              .join("|")
+          );
+          break;
+        case "genres":
+        case "steamspy_tags":
+          filter[key] = value[0].toUpperCase() + value.slice(1);
+          break;
+
+        default:
+          break;
+      }
+    }
+  }
+
+  console.log(filter);
+  // Query
+  const result = await db
+    .collection("games")
+    .find(filter)
+    .skip(limit * (page - 1))
+    .limit(limit)
+    .toArray();
+
   const length = result.length;
+
   res.send({ status: "success", data: result, length });
 });
+// router.get("/genres", async (req, res, next) => {
+//   const result = await db.collection("genres");
+//   const length = result.length;
+//   res.send({ status: "success", data: result, length });
+// });
 
 module.exports = router;
